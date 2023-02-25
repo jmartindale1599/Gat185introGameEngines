d12693d67b0af08f063b3e2f9a226f4c9701a403
jmartindale1599 <98477132+jmartindale1599@users.noreply.github.com>
2023-02-25 15:26:18 -0700

    added a animation aimer
    added a way to make aiming nicer looking, but I am having problems with the inventory making the gun appear sooo . . . great.

--- Assets/RollerGame/Scripts/Common/Inventory.cs
+++ Assets/RollerGame/Scripts/Common/Inventory.cs
@@ -6,13 +6,24 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
 	[SerializeField] InventoryData inventoryData;
+	[SerializeField] InventoryData defaultInventoryData;
 	[SerializeField] ItemData defaultItemData;
 
 	private Item[] items; // items that are a part of the owner game object (melee weapon, guns)
 	public Item currentItem { get; private set; }
 
-	private void Start()
-	{
+	private void Start(){
+
+		inventoryData.items.Clear();
+
+		foreach(var item in defaultInventoryData.items){
+
+			inventoryData.items.Add(item);
+
+		}
+
+
+
 		// search children for items (items that the owner game object possess)
 		items = GetComponentsInChildren<Item>(true);
 		if (defaultItemData != null)
@@ -61,6 +72,12 @@ public class Inventory : MonoBehaviour
 				currentItem?.Equip();
 			}
 		}
+	}
+
+	public void AddItem(ItemData itemData){
+
+		inventoryData.AddItem(itemData);
+
 	}
 
 	private Item GetItemFromItemData(ItemData itemData)
